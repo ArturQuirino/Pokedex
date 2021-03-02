281e7489b11ce328702e4e15d265ba06f1ee6217
Artur Quirino <artur.quirino@dtidigital.com.br>
2021-03-02 11:40:29 -0300

    Ajustes para buscar detalhes do pokemon no click do +

--- Frontend/pokedex/src/pages/repositories/api.js
+++ Frontend/pokedex/src/pages/repositories/api.js
@@ -4,3 +4,8 @@ export const obterTodosPokemons = async () => {
   const pokemonsResponse = await axios.get(`http://localhost:8080/pokemons`);
   return pokemonsResponse.data;
 }
+
+export const obterDadosPokemon = async (id) => {
+  const dadosPokemonResponse = await axios.get(`http://localhost:8080/pokemons/${id}`);
+  return dadosPokemonResponse.data;
+}
--- Frontend/pokedex/src/pages/shared/cardPokemon.jsx
+++ Frontend/pokedex/src/pages/shared/cardPokemon.jsx
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './cardPokemon.css';
+import { obterDadosPokemon } from '../repositories/api';
 
 function CardPokemon(props) {
+  const [modalAberto, setModalAberto] = useState(false);
+  const [types, setTypes] = useState([]);
+  const [abilities, setAbilities] = useState([]);
+  const [stat, setStat] = useState([]);
   const { pokemon } = props
   const { name, id } = pokemon;
   const urlImagem = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
 
+  const abrirDetalhesPokemon = async () => {
+    const dadosPokemon = await obterDadosPokemon(id);
+    const { types, abilities, stat } = dadosPokemon;
+    setTypes(types);
+    setAbilities(abilities);
+    setStat(stat);
+    console.log(stat);
+    setModalAberto(!modalAberto);
+  }
+
   return ( 
     <div className="card-externo">
         <img src={urlImagem} className="card-imagem" alt={name}/>
-        <div className="card-nome"> {name} </div>
+        <div className="card-nome">#{id} - {name} </div>
         <div className="card-dropdown">
-            <div className="card-botao"> + </div>
-            <div className="card-dropdown-aberto">
-                Teste
-            </div>
-        </div>    
+            <button className="card-botao" onClick={abrirDetalhesPokemon}> + </button>
+            {modalAberto && (
+              <div className="card-dropdown-aberto">
+                  <div>
+                    Type: {types.join(' / ')}
+                  </div>
+                  <div>
+                    Abilities: {abilities.join(' / ')}
+                  </div>
+                  <div>
+                    HP: {stat.find(s => s.name === 'hp').value}
+                  </div>
+                  <div>
+                    Attack: {stat.find(s => s.name === 'attack').value}
+                  </div>
+                  <div>
+                    Defense: {stat.find(s => s.name === 'defense').value}
+                  </div>
+                  <div>
+                    S.Atack: {stat.find(s => s.name === 'special-attack').value}
+                  </div>
+                  <div>
+                    S.Defence: {stat.find(s => s.name === 'special-defense').value}
+                  </div>
+                  <div>
+                    Speed: {stat.find(s => s.name === 'speed').value}
+                  </div>
+              </div>
+            )}
+
+            
+        </div>
     </div> 
   );
 }
